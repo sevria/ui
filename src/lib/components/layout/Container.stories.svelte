ac1b239a454b2dc4f1501e82ed85dc3edb4a08cd
Ukasyah <hi.ukasyah@gmail.com>
2025-07-27 18:21:02 +0700

    feat: bootstrap

---
+++ src/lib/components/layout/Container.stories.svelte
@@ -0,0 +1,18 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import Container from './Container.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Components/Layout/Container',
+    component: Container,
+    tags: ['autodocs']
+  });
+</script>
+
+<Story name="Demo">
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non consequatur, quo quod deleniti
+    eveniet nam, accusamus aliquam nobis libero laboriosam recusandae sunt facere obcaecati quaerat
+    magnam ipsa perspiciatis ad praesentium?
+  </p>
+</Story>
