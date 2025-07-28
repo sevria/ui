f284a14be27aefb0251582ac508fbc8aba1004f3
Ukasyah <hi.ukasyah@gmail.com>
2025-07-28 19:02:59 +0700

    feat: prepare for initial release

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,16 @@
 import type { Preview } from '@storybook/sveltekit';
+import { Provider } from '$lib/index.js';
 import '$lib/styles.css';
 
 const preview: Preview = {
+  decorators: [
+    (story) => ({
+      Component: Provider,
+      props: {
+        children: story
+      }
+    })
+  ],
   parameters: {
     controls: {
       matchers: {
