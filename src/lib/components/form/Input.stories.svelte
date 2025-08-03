066546dab3e1e51add51fc85f8210111b66badd1
Ukasyah <hi.ukasyah@gmail.com>
2025-08-03 13:16:01 +0700

    feat: input and link stories

---
+++ src/lib/components/form/Input.stories.svelte
@@ -0,0 +1,17 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import Input from './Input.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Components/Form/Input',
+    component: Input,
+    tags: ['autodocs'],
+    args: {
+      class: 'w-80'
+    }
+  });
+</script>
+
+<Story name="Basic" args={{ placeholder: 'Your name' }} />
+
+<Story name="Password" args={{ placeholder: 'Your password', type: 'password' }} />
---
+++ src/lib/components/navigation/Link.stories.svelte
@@ -0,0 +1,12 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import Link from './Link.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Components/Navigation/Link',
+    component: Link,
+    tags: ['autodocs']
+  });
+</script>
+
+<Story name="Demo" args={{ href: '/' }}>Go home</Story>
