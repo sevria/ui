f4b5f8776b2f981463317a2416c06623b77f9ee6
Ukasyah <hi@ukasyah.dev>
2026-04-21 05:51:10 +0700

    feat: add shadcn components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,19 @@
-<h1>Welcome to Sevria UI</h1>
+<script lang="ts">
+	import { Button } from '$lib';
+</script>
+
+<svelte:head>
+	<title>Sevria UI - Modern component library for Svelte</title>
+</svelte:head>
+
+<div class="mx-auto flex h-screen max-w-5xl flex-col items-center justify-center text-center">
+	<h1 class="text-4xl leading-snug font-semibold">Modern component library for Svelte</h1>
+	<p class="mt-3 max-w-xl text-lg text-muted-foreground">
+		Now you can focus on building great things and turning ideas into reality without ever worrying
+		about the UI again.
+	</p>
+	<div class="mt-6 flex gap-3">
+		<Button size="lg">Get started</Button>
+		<Button size="lg" variant="outline">Browse components</Button>
+	</div>
+</div>
